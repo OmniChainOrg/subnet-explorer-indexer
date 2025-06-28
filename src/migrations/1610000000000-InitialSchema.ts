00d4aee47e35ec9168fe884feb9552ba74aa3c9f
OmniChain <omnichain@icloud.com>
2025-06-28 17:13:02 +0200

    Update 1610000000000-InitialSchema.ts

--- src/migrations/1610000000000-InitialSchema.ts
+++ src/migrations/1610000000000-InitialSchema.ts
@@ -1,1 +1,25 @@
+import { MigrationInterface, QueryRunner } from "typeorm";
 
+export class InitialSchema1610000000000 implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+      CREATE TABLE "block" (
+        "height" integer PRIMARY KEY,
+        "hash" varchar NOT NULL,
+        "timestamp" timestamp NOT NULL
+      );
+      CREATE TABLE "transaction" (
+        "hash" varchar PRIMARY KEY,
+        "height" integer NOT NULL,
+        "body" jsonb NOT NULL
+      );
+    `);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+      DROP TABLE "transaction";
+      DROP TABLE "block";
+    `);
+  }
+}
